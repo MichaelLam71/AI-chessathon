dfd4c2d81029b2c9c4a5dfabd9375beba3a00c93
Mohil Garg <mr.mohilgarg@gmail.com>
2026-09-04 14:34:12 +0100

    Delete test_eval.py

--- test_eval.py
+++
@@ -1,7 +0,0 @@
-import chess
-from integrate_example import evaluate_learned
-
-print("start position:", evaluate_learned(chess.Board()))
-
-fen_up_a_queen = "rnb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-print("white up a queen:", evaluate_learned(chess.Board(fen_up_a_queen)))
